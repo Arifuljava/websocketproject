54478ca6b0ea9ef9be953431ee2776eee22e99da
Arif's Zone <arifulislam.java.c@gmail.com>
2023-09-06 01:02:22 -0700

    master

---
+++ lib/sockettest.dart
@@ -0,0 +1,27 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:web_socket_channel/web_socket_channel.dart';
+
+
+class sockettest extends StatefulWidget {
+  const sockettest({super.key});
+
+  @override
+  State<sockettest> createState() => _sockettestState();
+}
+
+class _sockettestState extends State<sockettest> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
